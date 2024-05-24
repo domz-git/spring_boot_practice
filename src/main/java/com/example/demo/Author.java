c30135f97aa00f18f51d2ff10ad48365ba9700c2
Dominik Filipović <domfil@enea.se>
2024-05-24 17:19:14 +0200

    added author and book classes

---
+++ src/main/java/com/example/demo/Author.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.util.Set;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+    
+}
---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Book {
+
+    private String title;
+    private String isbn;
+    
+}
